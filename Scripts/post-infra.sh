4a1d36108a5bdce56e27253a874af159f63e9a98
Sachin <sachinn.bashetti@gmail.com>
2026-04-30 12:48:38 +0530

    Remove helm version check from post-infra script
    Removed helm version check after installation.

--- Scripts/post-infra.sh
+++ Scripts/post-infra.sh
@@ -13,8 +13,6 @@ aws eks update-kubeconfig \
 echo "Installing Helm..."
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-helm version
-
 # ArgoCD Installation
 echo "Installing ArgoCD..."
 kubectl create namespace argocd || true
